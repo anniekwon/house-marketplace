5b2aa4bd87150c9e29cd23a4d737521849f49fd8
Annie Kwon <annie.s.kwon@gmail.com>
2022-12-17 16:56:57 -0600

    sign up and sign in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Explore from './pages/Explore'
-import Offers from './pages/Offers'
-import Profile from './pages/Profile'
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
-import Forgotpassword from './pages/Forgotpassword'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Explore from "./pages/Explore"
+import Offers from "./pages/Offers"
+import Profile from "./pages/Profile"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Forgotpassword from "./pages/Forgotpassword"
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Explore />} />
-          <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<Signin />} />
-          <Route path='/sign-in' element={<Signin />} />
-          <Route path='/sign-up' element={<Signup />} />
-          <Route path='/forgot' element={<Forgotpassword />} />
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Signin />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot" element={<Forgotpassword />} />
         </Routes>
         <Navbar />
       </Router>
