fb63e1853efceae51274d54c607fc2e34c81541f
Annie Kwon <annie.s.kwon@gmail.com>
2022-12-18 02:43:16 -0600

    commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,77 @@
 import { useState, useEffect } from "react"
-import { getAuth } from "firebase/auth"
+import { getAuth, updateProfile } from "firebase/auth"
+import { useNavigate, Link } from "react-router-dom"
+import { db } from "../firebase.config"
+import { updateDoc } from "firebase/firestore"
 
 function Profile() {
-  const [user, setUser] = useState(null)
   const auth = getAuth()
+  const [changeDetails, setChangeDetails] = useState(false)
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
 
-  useEffect(() => {
-    setUser(auth.currentUser)
-  }, [])
+  const { name, email } = formData
+  
+  const navigate = useNavigate()
 
-  return user ? <h1>{ user.displayName }</h1> : "Not Logged In"
+  const onLogout = () => {
+    auth.signOut()
+    navigate("/")
+  }
+
+  const onSubmit = () => {
+    console.log(123)
+  }
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+        Log Out
+      </button>
+    </header>
+
+    <main>
+      <div className="profileDetailsHeader">
+        <p className="profileDetailsText">
+          Personal Details
+        </p>
+        <p className="changePersonalDetails" 
+          onClick={() => {
+            changeDetails && onSubmit() 
+            setChangeDetails((prevState) => !prevState)
+          }}>
+          { changeDetails ? "done" : "change" }
+        </p>
+      </div>
+
+      <div className="profileCard">
+        <input type="text" id="name" 
+          className={!changeDetails ? "profileName" : "profileNameActive"} 
+          disabled={!changeDetails} 
+          value={name}
+          onChange={onChange}/>
+        <input type="text" id="email" 
+          className={!changeDetails ? "profileEmail" : "profileEmailActive"} 
+          disabled={!changeDetails} 
+          value={email}
+          onChange={onChange}/>
+      </div>
+
+      
+    </main>
+    </div>
+  )
 }
 
 export default Profile
