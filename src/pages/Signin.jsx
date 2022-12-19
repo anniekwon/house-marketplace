c4ccfad91da928f018327ecf083c9802e55d12a1
Annie Kwon <annie.s.kwon@gmail.com>
2022-12-18 23:29:47 -0600

    refractor authorization

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate} from "react-router-dom"
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilityIcon from "../assets/svg/visibilityIcon.svg"
+import OAuth from "../components/OAuth"
 
 function Signin() {
   const [ showPassword, setShowPassword ] = useState(false)
@@ -80,6 +81,8 @@ function Signin() {
             </button>
           </div>
         </form>
+
+        <OAuth />
 
         <Link to="/sign-up" className="registerLink">
           Sign Up Instead
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,7 +7,7 @@ import { setDoc, doc, serverTimestamp } from "firebase/firestore"
 import { db } from "../firebase.config"
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilityIcon from "../assets/svg/visibilityIcon.svg"
-
+import OAuth from "../components/OAuth"
 function Signup() {
   const [ showPassword, setShowPassword ] = useState(false)
   const [ formData, setFormData ] = useState({
@@ -99,6 +99,8 @@ function Signup() {
             </button>
           </div>
         </form>
+
+        <OAuth />
 
         <Link to="/sign-in" className="registerLink">
           Sign In Instead
